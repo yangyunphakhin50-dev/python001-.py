abc24c605f4972f703f61d400eb9c333105155eb
yangyunphakhin50-dev <yangyunphakhin50@gmail.com>
2026-03-06 13:29:16 +0700

    Create day1_print_input_.py

---
+++ day1_print_input_.py
@@ -0,0 +1,12 @@
+print("create your character!")
+
+name = input("what is your character called?")
+age = input("How old is your character's?")
+strengths = input("what are you character's strengths?")
+weaknesses = input("what are yor character's weaknesses?")
+
+print("your character's name is",name)
+print("your character is",age,"years old")
+print("strengths:",strengths)
+print("weaknesses:",weaknesses)
+print(name,"say, 'thanks for creating me!'")
